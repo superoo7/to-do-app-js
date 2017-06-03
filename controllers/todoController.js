6ce39e8c42457fcc98883529f35dfc0569615ea6
Johnson Lai <jlwhoo7@gmail.com>
2017-06-03 15:22:54 +0000

    upadted delete and post method

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,32 @@
+var bodyParser = require("body-parser");
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];     
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
 module.exports = function(app) {
+    
+    
+    
     // request templates
     app.get('/', function (req, res) {
         res.render('index');
     });
-    app.get('/todo', function (req, res) {
-        res.render('todo');
+    
+    app.get('/todo', urlencodedParser, function (req, res) {
+        res.render('todo', {todos: data});
+    });
+    app.post('/todo', function (req, res) {
+        data.push(req.body);
+        res.json(data);
+    });
+    app.delete('/todo/:item', function (req, res) {
+        data = data.filter(function(todo){
+            // when false, delete it
+            return todo.item.replace(/ /g, '-') !== req.params,item;
+        });
+        res.json(data);
     });
     
 };
