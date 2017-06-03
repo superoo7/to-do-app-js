4e4ccdd5fea9a85006e83358706cdab8878f2bfb
Johnson Lai <jlwhoo7@gmail.com>
2017-06-03 14:09:37 +0000

    added index.html file

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express        = require( 'express' );
 var http           = require( 'http' );
 
 var app            = express();
+
+app.set('view engine', 'ejs');
+
 app.set( 'port', process.env.PORT || 3001 );
+
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('index');
 });
 
 http.createServer( app ).listen( app.get( 'port' ), function (){
