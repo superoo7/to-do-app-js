057d0673c3bc20865ea49f95060104ca4c13c0d1
Johnson Lai <jlwhoo7@gmail.com>
2017-06-03 14:36:29 +0000

    done setup for controller

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var http           = require( 'http' );
 
 var app            = express();
 
+// controller
+var todoController = require('./controllers/todoController');
+
+
+
+// ejs (template engine)
 app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('./public'));
 
 app.set( 'port', process.env.PORT || 3001 );
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+// fire controller
+todoController(app);
+
+
+
 
 http.createServer( app ).listen( app.get( 'port' ), function (){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
