ea5c4092740f1c7cb4767844e7dbfb8e10dd5f4d
Johnson Lai <jlwhoo7@gmail.com>
2017-06-03 14:52:27 +0000

    added /todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,4 +3,8 @@ module.exports = function(app) {
     app.get('/', function (req, res) {
         res.render('index');
     });
+    app.get('/todo', function (req, res) {
+        res.render('todo');
+    });
+    
 };
