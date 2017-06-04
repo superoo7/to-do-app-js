479266c96511d61b43c603d7db38f67f59f5fb3f
Johnson Lai <jlwhoo7@gmail.com>
2017-06-04 00:48:14 +0000

    done to do app

--- README.md
+++ README.md
@@ -15,8 +15,16 @@ Welcome to your Node.js project on Cloud9 IDE!
 [Heroku Website](https://to-do-app-nodejs.herokuapp.com/)
 
 
+## Reference
+* [MongoDB server](http://mlab.com/)
+* [Express](https://www.npmjs.com/package/express)
+* [Nodemon](https://www.npmjs.com/package/nodemon)
+* [Body-Parser](https://www.npmjs.com/package/body-parser)
+* [ejs](https://www.npmjs.com/package/ejs)
+* [Ajax on jQuery](https://www.w3schools.com/jquery/jquery_ref_ajax.asp)
+* [Mongoose](https://www.npmjs.com/package/mongoose)
+* [API key hiding](https://github.com/sf-wdi-31/how-to-hide-secrets)
+
 ## Running the server
 
     nodemon app.js
-
-Once the server is running, open the project in the shape of 'https://projectname-username.c9users.io/'. As you enter your name, watch the Users list (on the left) update. Once you press Enter or Send, the message is shared with all connected clients.
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,27 @@
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+//
+var mlabConnection = process.env.MLABKEY;
+
+// connect to the database
+mongoose.connect(mlabConnection);
+
+//Create a schema (blueprint)
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+// Create a model
+var Todo = mongoose.model('Todo', todoSchema);
+
+// Create an item of the data and save it
+
+// add item
+// var itemOne = Todo({item: 'get flowers'}).save(function(err){
+//     if (err) throw err;
+//     console.log('item saved');
+// });
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];     
 
@@ -15,18 +38,32 @@ module.exports = function(app) {
     });
     
     app.get('/todo', function (req, res) {
-        res.render('todo', {todos: data});
+        // get data from mongodb and pass it to view
+        // specific which model
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        }); // find all
+        
     });
+    
     app.post('/todo', urlencodedParser, function (req, res) {
-        data.push(req.body);
-        res.json(data);
-    });
-    app.delete('/todo/:item', function (req, res) {
-        data = data.filter(function(todo){
-            // when false, delete it
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        // get data from view and add to mongodb
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
+    });
+    
+    app.delete('/todo/:item', function (req, res) {
+        
+        // delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if (err) throw err;
+            res.json(data);
+            
+            });
+        
     });
     
 };
